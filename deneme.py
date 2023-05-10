085942c181f8863c0da2bf1738097ce3fe054034
pandasspan <mbilaliscen@gmail.com>
2023-05-10 23:11:23 +0300

    10.05

--- deneme.py
+++ deneme.py
@@ -1,14 +1,16 @@
 
 
+import random
+
 class kullanici():
-    def __init__(self,ad,soyad, ana_para, kullanici_yasi, egitim_durumu, tecrube_yili):
+    def __init__(self,ad,soyad, ana_para, kullanici_yasi, egitim_durumu, tecrube_yili, kaybedebilecegi_oran):
         self.ad = ad
-        self.ad = soyad
+        self.soyad = soyad
         self.ana_para = ana_para
         self.kullanici_yasi = kullanici_yasi
         self.egitim_durumu = egitim_durumu
         self.tecrube_yili = tecrube_yili
-
+        self.kaybedebilecegi_oran = kaybedebilecegi_oran
         self.puan = 0 
 
 
@@ -54,27 +56,66 @@ class kullanici():
                 self.puan += 200
             print(self.puan)
 
+            if self.kaybedebilecegi_oran > 0.80:
+                self.puan += 200
+            elif self.kaybedebilecegi_oran > 0.50:
+                self.puan += 150
+            elif self.kaybedebilecegi_oran > 0.30:
+                self.puan += 100
+            elif self.kaybedebilecegi_oran > 0.20:
+                self.puan += 75
+            elif self.kaybedebilecegi_oran > 0.10:
+                self.puan += 50
+            elif self.kaybedebilecegi_oran > 0.05:
+                self.puan += 15
+
+
+            if self.puan >= 500:
+                print(f"Sayın {self.ad} {self.soyad} , yatırımcı profiliniz yüksek riskli yatırım yapmaya uygundur.")
+
+            elif self.puan >= 375:
+                print(f"Sayın {self.ad} {self.soyad} , yatırımcı profiliniz orta riskli yatırım yapmaya uygundur.")
+                        
+            elif self.puan >= 300:
+                print(f"Sayın {self.ad} {self.soyad} , yatırımcı profiliniz düşük riskli yatırım yapmaya uygundur.")
             
+            elif self.puan < 200:
+                print(f"Sayın {self.ad} {self.soyad} , ne yazık ki profiliniz yatırım tavsiyesi vermemiz için uygun değildir.")
+                
 
 class yatirimlar():
     def __init__(self, yatirim_adi, fiyati, risk_orani, vadesi):
+
         self.yatirim_adi = yatirim_adi
         self.fiyati = fiyati
         self.risk_orani = risk_orani
         self.vadesi = vadesi
 
+    yüksek_riskli_yatirimlar = ["NFT" , "Kripto Para" , "Start-Up Şirketleri" , "Gayrimenkul Yatırım Ortakları"]
+    orta_riskli_yatirimlar = ["Hisse Senetleri", "Yatırım Fonları" , "Emlak" , "Döviz Yatırımları"]
+    düşük_riskli_yatirimlar = ["Devlet Tahvilleri", "Repo-Ters Repo" , "Vadeli Mevduat" , "Para piyasası"]    
+    def tavsiye_et(risk_grubu):
+        yüksek_riskli_yatirimlar = ["NFT" , "Kripto Para" , "Start-Up Şirketleri" , "Gayrimenkul Yatırım Ortakları"]
+        orta_riskli_yatirimlar = ["Hisse Senetleri", "Yatırım Fonları" , "Emlak" , "Döviz Yatırımları"]
+        düşük_riskli_yatirimlar = ["Devlet Tahvilleri", "Repo-Ters Repo" , "Vadeli Mevduat" , "Para piyasası"]  
+        print(random.choice(risk_grubu))
+
 
 class danisman():
     def __init__(self,tavsiye_edilecek_uygun_yatirim):
         self.tavsiye_edilecek_uygun_yatirim = tavsiye_edilecek_uygun_yatirim
+    
+
+kullanici1 = kullanici("bilal", "işçen", 10000, 15, "üniversite", 2, 0.6)
 
 
-kullanici1 = kullanici("bilal", "işçen", 10000, 15, "üniversite", 2)
-kullanici1.kullanici_yasi = int(input("Yaşınızı giriniz: "))
-kullanici1.egitim_durumu = input("ilkokul/ortaokul/lise/üniversite/doktora: ")
-kullanici1.tecrube_yili = float(input("Kaç senelik tecrübeniz vardır: "))
-kullanici1.kullanici_profili_hesapla()
+#kullanici1.kullanici_yasi = int(input("Yaşınızı giriniz: "))
+#kullanici1.egitim_durumu = input("ilkokul/ortaokul/lise/üniversite/doktora: ")
+#kullanici1.tecrube_yili = float(input("Kaç senelik tecrübeniz vardır: "))
+#kullanici1.kullanici_profili_hesapla()
 
- 
+yatirimlar.tavsiye_et()
+
+
 
 
